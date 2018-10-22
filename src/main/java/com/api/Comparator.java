312fbd0a66c87aab7a9565584d996209d25ec8f8
Ibrahim <diihnini@gmail.com>
2018-10-23 00:05:40 +0100

    reversed method

--- src/main/java/com/api/Comparator.java
+++ src/main/java/com/api/Comparator.java
@@ -1,6 +1,26 @@
 package com.api;
 
+import static java.util.Objects.requireNonNull;
+
 @FunctionalInterface
-public interface Comparator<T>{
-    int compare(final T left,final T right);
+public interface Comparator<T> {
+    int compare(final T left, final T right);
+
+    default Comparator<T> reversed() {
+        return new Reversed<>(this);
+    }
+
+    class Reversed<T> implements Comparator<T> {
+        final Comparator<T> comparator;
+
+        Reversed(Comparator<T> original) {
+            requireNonNull(original);
+            this.comparator = original;
+        }
+
+        @Override
+        public int compare(T left, T right) {
+            return comparator.compare(right, left);
+        }
+    }
 }
