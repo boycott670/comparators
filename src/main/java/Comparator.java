ab901bf47f0197d322247236c26eb735ae65e98b
Ibrahim <diihnini@gmail.com>
2018-10-23 22:59:14 +0100

    changes to remote

--- src/main/java/Comparator.java
+++
@@ -1,5 +0,0 @@
-
-@FunctionalInterface
-public interface Comparator<T>{
-    int compare(final T left,final T right);
-}
--- src/main/java/com/api/Comparator.java
+++ src/main/java/com/api/Comparator.java
@@ -1,26 +1,11 @@
 package com.api;
 
-import static java.util.Objects.requireNonNull;
-
 @FunctionalInterface
 public interface Comparator<T> {
     int compare(final T left, final T right);
 
     default Comparator<T> reversed() {
-        return new Reversed<>(this);
+        return (left, right) -> compare(right, left);
     }
 
-    class Reversed<T> implements Comparator<T> {
-        final Comparator<T> comparator;
-
-        Reversed(Comparator<T> original) {
-            requireNonNull(original);
-            this.comparator = original;
-        }
-
-        @Override
-        public int compare(T left, T right) {
-            return comparator.compare(right, left);
-        }
-    }
 }
