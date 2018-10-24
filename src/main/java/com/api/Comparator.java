381cb92e67acac8bad5ede0f3cabf632502aacbd
Ibrahim <diihnini@gmail.com>
2018-10-24 01:37:03 +0100

    some others thenComparing
    # Conflicts:
    #	src/main/java/com/api/Comparator.java

--- src/main/java/com/api/Comparator.java
+++ src/main/java/com/api/Comparator.java
@@ -1,6 +1,9 @@
 package com.api;
 
 import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
 import static java.util.Objects.requireNonNull;
 
 @FunctionalInterface
@@ -24,6 +27,39 @@ public interface Comparator<T> {
         return (left, right) -> {
             int result = compare(left, right);
             return (result != 0) ? result : extractor.apply(left).compareTo(extractor.apply(right));
+        };
+    }
+
+    default <U> Comparator<T> thenComparing(Function<? super T, ? extends U> keyExtractor, Comparator<? super U> keyComparator) {
+        requireNonNull(keyComparator);
+        requireNonNull(keyExtractor);
+        return (left, right) -> {
+            int result = compare(left, right);
+            return (result != 0) ? result : keyComparator.compare(keyExtractor.apply(left), keyExtractor.apply(right));
+        };
+    }
+
+    default Comparator<T> thenComparingDouble(ToDoubleFunction<? super T> keyExtractor) {
+        requireNonNull(keyExtractor);
+        return (left, right) -> {
+            int result = compare(left, right);
+            return (result != 0) ? result : Double.compare(keyExtractor.applyAsDouble(left), keyExtractor.applyAsDouble(right));
+        };
+    }
+
+    default Comparator<T> thenComparingInt(ToIntFunction<? super T> keyExtractor) {
+        requireNonNull(keyExtractor);
+        return (left, right) -> {
+            int result = compare(left, right);
+            return (result != 0) ? result : Integer.compare(keyExtractor.applyAsInt(left), keyExtractor.applyAsInt(right));
+        };
+    }
+
+    default Comparator<T> thenComparingLong(ToLongFunction<? super T> keyExtractor) {
+        requireNonNull(keyExtractor);
+        return (left, right) -> {
+            int result = compare(left, right);
+            return (result != 0) ? result : Long.compare(keyExtractor.applyAsLong(left), keyExtractor.applyAsLong(right));
         };
     }
 }
